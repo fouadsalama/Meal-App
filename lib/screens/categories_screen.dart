54f9d192949b81164860524d3d2e857e17e8b6e7
Fouad Salama <fs2030565@gmail.com>
2023-01-01 03:04:08 +0200

    Meal App

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:mealapp/dummy_data.dart';
+import 'package:mealapp/widget/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(title: Text('meal'),),
+      body: GridView(
+        padding:  EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES.map(
+              (catData) => CategoryItem(
+                catData.id,
+                catData.title,
+                catData.color,
+              ),
+            ).toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
